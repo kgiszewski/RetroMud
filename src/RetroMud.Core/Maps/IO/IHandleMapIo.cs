20e3c70911aa855eeb968886f0ce1239d8d7afb2
Kevin Giszewski <kevin@giszewski.com>
2017-06-21 18:23:55 -0400

    Start map IO

---
+++ src/RetroMud.Core/Maps/IO/IHandleMapIo.cs
@@ -0,0 +1,8 @@
+﻿namespace RetroMud.Core.Maps.IO
+{
+    public interface IHandleMapIo
+    {
+        IMap LoadMap();
+        void SaveAlteredMapState();
+    }
+}
---
+++ src/RetroMud.Core/Maps/IO/MapInputOutput.cs
@@ -0,0 +1,15 @@
+﻿namespace RetroMud.Core.Maps.IO
+{
+    public class MapInputOutput : IHandleMapIo
+    {
+        public IMap LoadMap()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveAlteredMapState()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
